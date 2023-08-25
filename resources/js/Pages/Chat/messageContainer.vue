3084f7ffee23219143cf3a6a293be234fd9e8b56
David Dume <dumedavid11@gmail.com>
2023-08-25 22:33:39 +0200

    display messages

--- resources/js/Pages/Chat/messageContainer.vue
+++ resources/js/Pages/Chat/messageContainer.vue
@@ -1,9 +1,18 @@
 <template>
-    <div>Message Container</div>
+    <div class="h-96 w-full">
+        <div class="h-full p-2 flex flex-col-reverse overflow-scroll">
+            <div v-for="(message, index) in messages" :key="index">
+                <MessageItem :message="message"></MessageItem>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-    export default {
+import MessageItem from './MessageItem.vue';
 
-    }
+    export default {
+    props: ['messages'],
+    components: { MessageItem }
+}
 </script>
